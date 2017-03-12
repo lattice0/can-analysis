85e8b21b9ecd6a48665902d1a6602a16d4543850
Lucas Zanella <me@lucaszanella.com>
2017-03-12 20:18:29 -0300

    first commit

---
+++ serial_echo.py
@@ -0,0 +1,33 @@
+import serial
+import getch
+import time
+import threading
+
+ser = serial.Serial('/dev/ttyACM0', 115200)
+known = []
+f = open('trace.txt', 'w')
+
+def write_break_line():
+    while True:
+        key = ord(getch.getch())
+        if key==98:
+            f.write("---\n")
+            print("---")
+        if key==113:
+            exit()
+
+t = threading.Thread(target=write_break_line)
+t.start()
+while 1:
+    line = str(ser.readline())
+    a = line.split(',')
+    id = a[0].replace('ID: ', '')
+    data = a[1].replace('Data: ', '')
+    data = data.split(' ')
+    if id not in known:
+        print(id + ":" + data)
+        known.append(id)
+        f.write(id+"\n")  # python will convert \n to os.linesep
+
+f.close()  # you can omit in most cases as the destructor will call it
+ser.close()
