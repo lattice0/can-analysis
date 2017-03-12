7b43484027402a13afd3ca9038d8be37f24e4090
Lucas Zanella <me@lucaszanella.com>
2017-03-12 20:42:42 -0300

    ignores can init message

--- serial_echo.py
+++ serial_echo.py
@@ -20,14 +20,17 @@ t = threading.Thread(target=write_break_line)
 t.start()
 while 1:
     line = str(ser.readline())
-    a = line.split(',')
-    id = a[0].replace('ID: ', '')
-    data = a[1].replace('Data: ', '')
-    data = data.split(' ')
-    if id not in known:
-        print(id + ":" + data)
-        known.append(id)
-        f.write(id+"\n")  # python will convert \n to os.linesep
+    if not 'CAN' in line:
+        a = line.split(',')
+        id = a[0].replace('ID: ', '')
+        data = a[1].replace('Data: ', '')
+        data = data.split(' ')
+        if id not in known:
+            print(id + ":" + data)
+            known.append(id)
+            f.write(id+"\n")  # python will convert \n to os.linesep
+    else:
+        line
 
 f.close()  # you can omit in most cases as the destructor will call it
 ser.close()
