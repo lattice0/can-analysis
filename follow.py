20899eed71657bede185298c4265019937039044
Lucas Zanella <me@lucaszanella.com>
2017-03-12 22:26:02 -0300

    added memory to follow.py

--- follow.py
+++ follow.py
@@ -7,23 +7,33 @@ parser.add_argument('-i','--ids', nargs='+', help='List of IDs to follow')
 args = parser.parse_args()
 ids = vars(args)['ids']
 
+memory = {}
 print("Following ids: " + str(ids))
-
-while 1:
+for i in ids:
+    memory[i] = ""
+print(memory)
+while True:
     line = str(ser.readline())
     if not 'CAN' in line:
         try:
             a = line.split(',')
-            id = a[0].replace('ID: ', '')
-            data = a[1].replace('Data: ', '')
-            data = data.split(' ')
+            id = a[0].split(' ')[1]
+            #data = a[1].replace('Data: ', '')
+            #data = data.split(' ')
             if id in ids:
-                print(line)
-            print('id not:'+id)
-            print('id type:'+type(id))
-            print('ids:'+str(ids))
-        except:
+                if not line in memory[id]:
+                    memory[id] = line
+                    print(line)
+                else:
+                    pass
+            else:
+                #print('id not:'+id)
+                #print('id type:'+str(type(id)))
+                #print('ids:'+str(ids))
+                pass
+        except Exception as e:
             #f.write("ERROR: " + str(line))
-            print("ERROR: " + str(line))
+            print("ERROR: " + str(line) + " error: " +str(e))
+            pass
     else:
         print(line)
