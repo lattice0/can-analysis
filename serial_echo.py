e282661f811942fd7a62b8fb4f2ee519878cb6ac
Lucas Zanella <me@lucaszanella.com>
2017-03-12 21:08:49 -0300

    changes

--- serial_echo.py
+++ serial_echo.py
@@ -6,6 +6,8 @@ import threading
 ser = serial.Serial('/dev/ttyACM0', 115200)
 known = []
 f = open('trace.txt', 'w')
+id_ = True
+line_ = False
 
 def write_break_line():
     while True:
@@ -16,19 +18,29 @@ def write_break_line():
         if key==113:
             exit()
 
-t = threading.Thread(target=write_break_line)
-t.start()
+#t = threading.Thread(target=write_break_line)
+#t.start()
 while 1:
     line = str(ser.readline())
     if not 'CAN' in line:
-        a = line.split(',')
-        id = a[0].replace('ID: ', '')
-        data = a[1].replace('Data: ', '')
-        data = data.split(' ')
-        if id not in known:
-            print(id + ":" + data)
-            known.append(id)
-            f.write(id+"\n")  # python will convert \n to os.linesep
+        try:
+            a = line.split(',')
+            id = a[0].replace('ID: ', '')
+            data = a[1].replace('Data: ', '')
+            data = data.split(' ')
+        except:
+            f.write("ERROR: " + str(line))
+            print("ERROR: " + str(line))
+        if id_:
+            if not id in known:
+                print(str(id) + ":" + str(data))
+                known.append(id)
+                f.write(str(id)+"\n")  # python will convert \n to os.linesep
+        if line_:
+            if not line in known:
+                print(line)
+                known.append(line)
+                f.write(str(line)+"\n")  # python will convert \n to os.linesep
     else:
         print(line)
 
